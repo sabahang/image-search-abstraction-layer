a445a7fa57b1e4c632bf544c997bae5003c80a89
Saba Ahang <hesabb@gmail.com>
2016-03-20 18:08:21 +0000

    heroku: added mongolab url and production/development settings

--- server.js
+++ server.js
@@ -10,17 +10,26 @@ var mongoose = require('mongoose');
 var app = express();
 
 
-mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URI );
+
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  mongoose.connect(process.env.MONGO_URI );
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+  mongoose.connect(process.env.MONGOLAB_URI);
+});
 
 routes(app);
 
-//var port = process.env.PORT || 8080;
-var port = 8080;
+var port = process.env.PORT || 8080;
+//var port = 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
