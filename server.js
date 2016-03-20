5d1d5e07bf62d27237a007ebb29c0f827857e9b9
Saba Ahang <hesabb@gmail.com>
2016-03-20 17:36:22 +0000

    adding mongolab server url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var mongoose = require('mongoose');
 var app = express();
 
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URI );
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
